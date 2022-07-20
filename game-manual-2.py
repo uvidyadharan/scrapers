7be97385a71ab9dafe54fa37b57017c0b52eb746
Uday Vidyadharan <55924756+uvidyadharan@users.noreply.github.com>
2022-07-19 23:17:45 -0400

    Update game-manual-2.py

--- game-manual-2.py
+++ game-manual-2.py
@@ -1,1 +1,34 @@
+import wget
+import hashlib
+import os
+
+wget.download("https://www.firstinspires.org/sites/default/files/uploads/resource_library/ftc/game-manual-part-2-remote.pdf", "gm2.pdf")
+
+buffer = 100000
+sha256 = hashlib.sha256()
+oldHash = None
+
+with open("gm1.pdf", 'rb') as file:
+  while True:
+    data = file.read(buffer)
+    if not data:
+      break
+    sha256.update(data)
+    
+
+try:
+  with open("gm2.pdf.sha256", "r") as hashFile:
+    oldHash = hashFile.read()
+except:
+  oldHash = ""
+  
+if oldHash != sha256.hexdigest():
+  os.environ["UPDATED"] = "1"
+  print(1)
+  with open("gm2.pdf.sha256", "w") as hashFile:
+    hashFile.write(sha256.hexdigest())
+else:
+  os.environ["UPDATED"] = "0"
+  print(1)
+  
 
